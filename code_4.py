9c0033fc13b90eedadcd0122d60d7e225f769824
shimmy2468 <shimmy2200@gmail.com>
2023-09-28 11:00:05 -0600

    code 4 first try

--- code_4.py
+++ code_4.py
@@ -2,17 +2,43 @@ import csv
 import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
 
+df = pd.read_csv(big_mac_file)
+
 def get_big_mac_price_by_year(year,country_code):
-    pass # Remove this line and code your function
+    query = f"(iso_a3 == '{country_code.upper()}' and date <= '{year}-12-31' and date >= '{year}-01-01')"
+    df_1 = df.query(query)
+    price_by_year = df_1['dollar_price'].mean()
+
+    return round(price_by_year, 2)
 
 def get_big_mac_price_by_country(country_code):
-    pass # Remove this line and code your function
+    query = f"(iso_a3 == '{country_code.upper()}')"
+    df_2 = df.query(query)
+    price_by_country= df_2['dollar_price'].mean()
+   
+    return round(price_by_country, 2)
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    query = f"(date <= '{year}-12-31' and date >= '{year}-01-01')"
+    df_3 = df.query(query)
+    index_min_value = df_3['dollar_price'].idxmin()
+    output = df_3.loc[index_min_value]
+
+    return f"{output['name']}({output['iso_a3']}): ${round(output['dollar_price'], 2)}" 
+
 
 def get_the_most_expensive_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    query = f"(date <= '{year}-12-31' and date >= '{year}-01-01')"
+    df_4 = df.query(query)
+    index_max_value = df_4['dollar_price'].idxmax()
+    output = df_4.loc[index_max_value]
+
+    return f"{output['name']}({output['iso_a3']}): ${round(output['dollar_price'], 2)}" 
 
 if __name__ == "__main__":
-    pass # Remove this line and code your user interface
+    print(get_big_mac_price_by_year(2009, "mex"))
+    print(get_big_mac_price_by_country("rus"))
+    print(get_the_cheapest_big_mac_price_by_year(2016))
+    print(get_the_most_expensive_big_mac_price_by_year(2016))
+
+
